23a205523b4113507ad0f0fd4c8b549d324cb163
HamzaMax <hamza1mks1@gmail.com>
2025-05-23 16:30:51 +0300

    test

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
       builder: (_, child) {
         return GetMaterialApp(
           debugShowCheckedModeBanner: false,
-          theme: ThemeData(fontFamily: 'Calibr'),
+          theme: ThemeData(fontFamily: 'Calib'),
           home: Login(),
         );
       },
